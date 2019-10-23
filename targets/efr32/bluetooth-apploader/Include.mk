d398ef6922db0aae07bf3c151b5c16851a10ef8b
Štefan Šimek <simek@triaxis.sk>
2019-10-24 00:37:40 +0200

    efr32: binary apploader support

---
+++ targets/efr32/bluetooth-apploader/Include.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (c) 2019 triaxis s.r.o.
+# Licensed under the MIT license. See LICENSE.txt file in the repository root
+# for full license information.
+#
+# efr32/bluetooth-apploader/Include.mk
+#
+
+ADDITIONAL_BLOBS += $(EFM32_SDK_BT_DEV)GCC/binapploader.o
+
+TARGETS += efr32-apploader
