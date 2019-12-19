3ab444e41fe90fe080ee56ec18ae06ce75a0aec1
Štefan Šimek <simek@triaxis.sk>
2019-12-19 23:30:38 +0100

    bluetooth: preliminary support for libbluetooth 2.13.x

--- targets/efr32/bluetooth/stubs.cpp
+++ targets/efr32/bluetooth/stubs.cpp
@@ -22,4 +22,21 @@ void CRYPTO0_IRQHandler()
     ASSERT(0);
 }
 
+void sl_sleeptimer_init()
+{
+    // timer initialization is kernel's responsibility
+}
+
+void sl_sleeptimer_restart_timer()
+{
+    // must not be called (used only by gecko_wait_event)
+    ASSERT(0);
+}
+
+uint32_t sl_sleeptimer_get_tick_count()
+{
+    // the one true time source
+    return MONO_CLOCKS;
+}
+
 END_EXTERN_C
